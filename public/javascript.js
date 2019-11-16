b4b23b8cc43cded205c52635d4834b310a18a0a2
Dan Dismuke <dandismuke@gmail.com>
2019-11-16 14:54:05 -0800

    Functional prototype with one test audio file.

--- public/javascript.js
+++ public/javascript.js
@@ -2,4 +2,15 @@ $(document).ready(function() {
   var fartBtn = $("<div>");
   fartBtn.attr("class", "farts");
   $(".container").append(fartBtn);
+
+  $(".farts").on("click", function() {
+    console.log("clicked");
+    playAudio();
+  });
+
+  function playAudio() {
+    var audioElement = document.createElement("audio");
+    audioElement.setAttribute("src", "public/test.mp3");
+    audioElement.play();
+  }
 });
